cf592eaf0aa20ae118df0173f92446bc1333cf85
Fabrizio Monaco <fabrimagic@me.com>
2017-12-22 14:20:58 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongodb
+apt-get install -y python-pip
+pip install -r requirements.txt
